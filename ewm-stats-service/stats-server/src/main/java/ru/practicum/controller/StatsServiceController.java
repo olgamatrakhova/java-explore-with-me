821d753ebfbc87e1c47cff931cf80c624ac43369
olgamatrakhova <gluhova.olga@gmail.com>
2024-02-01 20:10:05 +0300

    Сервис Статистики

--- ewm-stats-service/stats-server/src/main/java/ru/practicum/controller/StatsServiceController.java
+++ ewm-stats-service/stats-server/src/main/java/ru/practicum/controller/StatsServiceController.java
@@ -1,4 +1,4 @@
-package ru.practicum;
+package ru.practicum.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -6,7 +6,11 @@ import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import ru.practicum.dto.StatsDto;
 import ru.practicum.dto.StatsResponseDto;
 import ru.practicum.service.StatsService;
