7f001a5a950af49f9f4a4ed4a5bbf9b691709103
olgamatrakhova <gluhova.olga@gmail.com>
2024-02-02 23:41:00 +0300

    Сервис Статистики

--- ewm-stats-service/stats-server/src/main/java/ru/practicum/repository/StatsServiceRepository.java
+++ ewm-stats-service/stats-server/src/main/java/ru/practicum/repository/StatsServiceRepository.java
@@ -12,28 +12,28 @@ import java.util.List;
 @Repository
 public interface StatsServiceRepository extends JpaRepository<Stats, Long> {
     @Query("select new ru.practicum.dto.StatsResponseDto(s.ip, s.uri, count(distinct s.ip)) " +
-            "from stats as s " +
+            "from Stats as s " +
             "where s.timestamp between ?1 and ?2 " +
             "group by s.ip, s.uri " +
             "order by count(distinct s.ip) desc")
     List<StatsResponseDto> getAllByTimestampBetweenStartAndEndWithUniqueIp(LocalDateTime start, LocalDateTime end);
 
     @Query("select new ru.practicum.dto.StatsResponseDto(s.ip, s.uri, count(s.ip)) " +
-            "from stats as s " +
+            "from Stats as s " +
             "where s.timestamp between ?1 and ?2 " +
             "group by s.ip, s.uri " +
             "order by count(s.ip) desc ")
     List<StatsResponseDto> getAllByTimestampBetweenStartAndEndWhereIpNotUnique(LocalDateTime start, LocalDateTime end);
 
     @Query("select new ru.practicum.dto.StatsResponseDto(s.ip, s.uri, count(distinct s.ip)) " +
-            "from stats as s " +
+            "from Stats as s " +
             "where s.timestamp between ?1 and ?2 and s.uri in ?3 " +
             "group by s.ip, s.uri " +
             "order by count(distinct s.ip) desc ")
     List<StatsResponseDto> getAllByTimestampBetweenStartAndEndWithUrisUniqueIp(LocalDateTime start, LocalDateTime end, List<String> uris);
 
     @Query("select new ru.practicum.dto.StatsResponseDto(s.ip, s.uri, count(s.ip)) " +
-            "from stats as s " +
+            "from Stats as s " +
             "where s.timestamp between ?1 and ?2 and s.uri in ?3 " +
             "group by s.ip, s.uri " +
             "order by count(s.ip) desc ")
