b3d001b7276aeeb9c2e5a6da4ae21166cfe6914c
olgamatrakhova <gluhova.olga@gmail.com>
2024-02-07 18:05:43 +0300

    Основной сервис

--- ewm-main-service/src/main/java/ru/practicum/users/repository/UserRepository.java
+++ ewm-main-service/src/main/java/ru/practicum/users/repository/UserRepository.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Repository;
 import ru.practicum.users.model.User;
 
 import java.util.List;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     @Query("select u from User u where u.id IN ?1 ")
